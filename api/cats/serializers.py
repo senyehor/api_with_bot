6ba0392f1d2bb660416896a74c27268e78e60136
Yehor Sencha <senyehor@gmail.com>
2024-07-30 16:20:54 +0300

    feat: make serializers include readonly id

--- api/cats/serializers.py
+++ api/cats/serializers.py
@@ -6,7 +6,8 @@ from .models import Breed, Cat
 class BreedSerializer(ModelSerializer):
     class Meta:
         model = Breed
-        fields = ('name',)
+        fields = ('id', 'name',)
+        read_only_fields = ('id',)
 
 
 class CatSerializer(ModelSerializer):
@@ -14,5 +15,5 @@ class CatSerializer(ModelSerializer):
 
     class Meta:
         model = Cat
-        fields = ('name', 'breed', 'description_from_owner')
-        read_only_fields = ('breed',)
+        fields = ('id', 'name', 'breed', 'description_from_owner')
+        read_only_fields = ('id', 'breed',)
