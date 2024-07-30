5abf1b07ce56e523eab1cd4e056d4ec150efdec7
Yehor Sencha <senyehor@gmail.com>
2024-07-30 16:27:57 +0300

    feat: add serving static files for development

--- api/api/urls.py
+++ api/api/urls.py
@@ -1,5 +1,12 @@
+from django.conf.urls.static import static
 from django.urls import include, path
+
+from api.settings import DEBUG, STATIC_URL
 
 urlpatterns = [
     path('api/', include('cats.urls')),
+
 ]
+
+if DEBUG:
+    urlpatterns += static(STATIC_URL)
