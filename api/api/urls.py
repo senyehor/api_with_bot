f475b1ce96e117f2c7494075ef551898c60fe5cd
Yehor Sencha <senyehor@gmail.com>
2024-07-30 16:28:16 +0300

    feat: add docs as a default page

--- api/api/urls.py
+++ api/api/urls.py
@@ -1,11 +1,13 @@
 from django.conf.urls.static import static
 from django.urls import include, path
+from django.views.generic import RedirectView
+from rest_framework.reverse import reverse_lazy
 
 from api.settings import DEBUG, STATIC_URL
 
 urlpatterns = [
     path('api/', include('cats.urls')),
-
+    path('', RedirectView.as_view(url=reverse_lazy('docs')))
 ]
 
 if DEBUG:
