d5f998bd950a16a579e9a4c862d39ef92073b8c5
Yehor Sencha <senyehor@gmail.com>
2024-07-30 13:32:16 +0300

    feat: add cats app

---
+++ api/cats/models.py
@@ -0,0 +1,27 @@
+from django.db.models import CharField, ForeignKey, Model, RESTRICT
+
+
+class Cat(Model):
+    breed = ForeignKey(
+        'Breed',
+        related_name='cats',
+        on_delete=RESTRICT
+    )
+    name = CharField(
+        null=False,
+        blank=False,
+        max_length=255
+    )
+    description_from_owner = CharField(
+        null=False,
+        blank=False,
+        max_length=1020
+    )
+
+
+class Breed(Model):
+    name = CharField(
+        null=False,
+        blank=False,
+        max_length=255
+    )
