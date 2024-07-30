0e374f1866c7849f5f00d4109116c2ca019fe756
Yehor Sencha <senyehor@gmail.com>
2024-07-30 14:47:38 +0300

    feat: create random cats view

--- api/cats/urls.py
+++ api/cats/urls.py
@@ -1,1 +1,7 @@
-urlpatterns = []
+from django.urls import path
+
+from cats.views import RandomCatsListView
+
+urlpatterns = [
+    path('random-cats', RandomCatsListView.as_view(), name='random-cats')
+]
--- api/cats/views.py
+++ api/cats/views.py
@@ -1,1 +1,19 @@
+from rest_framework.generics import ListAPIView
 
+from cats.logic import DEFAULT_CATS_AMOUNT, parse_int, RandomCatsQuerier
+from cats.models import Cat
+from cats.serializers import CatSerializer
+
+
+class RandomCatsListView(ListAPIView):
+    serializer_class = CatSerializer
+    cats_querier = RandomCatsQuerier(Cat)
+
+    def get_queryset(self):
+        cats_amount = self.__extract_cats_amount_to_query()
+        return self.cats_querier.get_random_cats(cats_amount, fail_on_not_enough_cats=False)
+
+    def __extract_cats_amount_to_query(self):
+        if cats_amount := self.request.GET.get('cats_amount', None):
+            return parse_int(cats_amount, 'the cats_amount you provided failed to parse')
+        return DEFAULT_CATS_AMOUNT
